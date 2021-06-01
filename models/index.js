ce9dc2f511ed931cb856ce836e2cea024da33b2f
Erick Flore <ferick8246@gmail.com>
2021-05-31 21:15:14 -0500

    Products

--- models/index.js
+++ models/index.js
@@ -12,9 +12,11 @@ Product.belongsTo(Category, {
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
-
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
