d84adf3911b8ca2aff4fc3fb03d49d80b1c491aa
Erick Flore <ferick8246@gmail.com>
2021-05-31 21:53:49 -0500

    Product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,37 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+// be sure to include its associated Product data
+router.get('/:id', (req, res) => {try{
+  const productData = await Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  res.status(200).json(productData)
+}catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
-
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
 });
 
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
 });
 
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
 });
 
 module.exports = router;
