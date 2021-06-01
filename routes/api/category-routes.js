7eceb10094c538f737292a780861f6c73276b406
Erick Flore <ferick8246@gmail.com>
2021-05-31 21:29:40 -0500

    new cat

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,8 +38,16 @@ router.get('/:id', (req, res) => {
       ] 
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', (req, res) => { try{
+  const categoryData = await Category.create({
+    category_name: req.body.category_name
+  })
+
+  res.status(200).json(categoryData)
+}catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.put('/:id', (req, res) => {
