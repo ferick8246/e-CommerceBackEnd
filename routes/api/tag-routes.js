8bf81dbc2da3c224e9d9eaf524def8b95e580c47
Erick Flore <ferick8246@gmail.com>
2021-05-31 21:56:57 -0500

    /api/tags` endpoint

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,25 +1,30 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+// The `/api/tags` endpoint
+// find all tags
+// be sure to include its associated Product data
+
+router.get('/', (req, res) => {
+  try{
+    const tagData = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+    })
+    res.status(200).json(productData)
+  }catch (err) {
+    res.status(500).json(err);
+  }
+
+});
 // find a single tag by its `id`
 // be sure to include its associated Product data
-router.get('/:id', (req, res) => {try{
-  const productData = await Product.findAll({
-    include: [
-      {
-        model: Category,
-        attributes: ['id', 'category_name']
-      },
-      {
-        model: Tag,
-        attributes: ['id', 'tag_name']
-      }
-    ]
-  })
-  res.status(200).json(productData)
-}catch (err) {
-  res.status(500).json(err);
-}
+
+router.get('/:id', (req, res) => {
 });
 
 // create a new tag
