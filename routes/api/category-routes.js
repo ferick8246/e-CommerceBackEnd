39b803d5c2abf43181d0687621d6d81bb063d944
Erick Flore <ferick8246@gmail.com>
2021-05-31 21:27:38 -0500

    id value associated Products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,10 +21,21 @@ router.get('/', (req, res) => {
     res.status(500).json(err);
   }
 });
+// find one category by its `id` value
+// be sure to include its associated Products
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ] 
 });
 
 router.post('/', (req, res) => {
