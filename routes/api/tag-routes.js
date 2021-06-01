9296958c85ad78102cce62892b01a0763f44562b
Erick Flore <ferick8246@gmail.com>
2021-05-31 22:00:06 -0500

    `id` value

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,6 +61,20 @@ router.post('/', (req, res) => {
 
 // update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
+  try{
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found'})
+    }
+    res.status(200).json(tagData)
+  }catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // delete on tag by its `id` value
